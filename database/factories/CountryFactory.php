9b7f377fdf2a496d382ae639093c86dd2ad770c4
alkarpova <sashulja01@gmail.com>
2024-09-26 16:02:20 +0300

    Add Country [Model, Migration, Factory]

---
+++ database/factories/CountryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Country>
+ */
+class CountryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->country,
+            'iso2' => $this->faker->countryCode,
+            'iso3' => $this->faker->countryISOAlpha3,
+            'status' => $this->faker->boolean,
+        ];
+    }
+}
---
+++ database/migrations/2024_09_26_154440_create_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name')->unique()->index();
+            $table->string('iso2', 2);
+            $table->string('iso3', 3);
+            $table->boolean('status')->default(true)->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
