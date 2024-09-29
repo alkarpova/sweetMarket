01b126d361ce485c08bd4fae2615bbe9afbf5ff6
alkarpova <sashulja01@gmail.com>
2024-09-29 22:09:34 +0300

    Add Theme [Model, Migration, Factory]

---
+++ app/Models/Theme.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Theme extends Model
+{
+    use HasFactory, HasUlids, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+            'status' => 'boolean',
+        ];
+    }
+
+    /**
+     * Scope a query to status.
+     */
+    public function scopeStatus(Builder $query, bool $status = true): Builder
+    {
+        return $query->where('status', $status);
+    }
+}
