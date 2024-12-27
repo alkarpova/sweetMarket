cb0b788b5be5eab59f855b19e70192e4c2d971dd
alkarpova <sashulja01@gmail.com>
2024-12-27 20:23:00 +0200

    Fix Navbar Mobile

--- resources/views/livewire/components/categories.blade.php
+++ resources/views/livewire/components/categories.blade.php
@@ -1,5 +1,5 @@
 <nav class="container mx-auto px-5 my-4">
-    <div class="grid grid-cols-6 gap-4">
+    <div class="grid lg:grid-cols-6 gap-4">
         @foreach($this->categories as $category)
             <a
                 class="flex items-center justify-center gap-4 px-5 py-3 rounded-lg font-semibold hover:bg-green-900 hover:hover:text-white {{ $this->isActiveCategory($category->id) ? 'bg-green-900 text-white' : 'bg-white' }}"
